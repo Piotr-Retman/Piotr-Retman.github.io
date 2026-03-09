5774dd12446db85d42f9927211dfca4ea39d8b1d
Piotr <piotr.retmanczyk@gmail.com>
2026-03-09 11:53:04 +0100

    **Refactor: Replace legacy code with modernized UI and translations**
    - Removed outdated JavaScript (`retman.js`) and legacy HTML structure.
    - Introduced a refreshed design with a new CSS stylesheet (`modern.css`) emphasizing responsiveness and accessibility.
    - Added multilingual support via `modern.js`, including English and Polish translations.
    - Simplified navigation and redesigned sections (About, After Hours, Contact).

---
+++ js/modern.js
@@ -0,0 +1,126 @@
+const translations = {
+    en: {
+        nav_about: "About me",
+        nav_after: "After hours",
+        nav_contact: "Contact",
+        hero_hi: "Hi! It is good to see you.",
+        hero_welcome: "Please feel welcome on my page!!",
+        about_title: "About me",
+        about_who: "Who am I?",
+        about_p1: "Hi! My name is Piotr Retmańczyk and I am programmer for more than ten years. In everyday work I am Senior Software Engineer",
+        about_p2: "Doing this business is my choice, my passion and this is what I want in everyday life. I caught the bug when I was studying during my first degree. This is what I love to do and improving skills which are strictly connected with my passion is the one of the most important thing in my life. My journey started in 2011 with Python scripts and after this moment during the years I had opportunity to work with plenty of technologies and languages.",
+        about_day_title: "Day by day",
+        about_day_p: "In everyday coding I use languages like Java or JS which are strictly connected with backend in my case. Mostly, my career is based on being typical backend developer. But I am not telling \"no\" to any technologies which let me create mobile, desktop and web applications.",
+        after_title: "After hours",
+        after_q: "What do I do after job?",
+        after_p1: "Like greatest minds in \"IT\" industry suggest - I am improving all the time my languages set, soft skills and ability to create architectural solutions.",
+        after_p2: "I also found the way of great entertainment and hobby in creating games. I have no big success on my account but you can find one title online. If you are interested please check <a href=\"https://seriousappsdev.itch.io/mirrorreflection\" target=\"_blank\">this page</a>",
+        after_hobby: "Hobby & Interests",
+        after_music: "Music",
+        after_music_p1: "I am big enthusiast of music from 70,80,90's. I can place here long list of bands but it is so long that I will stay with a few most liked by me for which I come back very soon.",
+        after_music_p2: "New Wave, Synth Pop and Rock are the most loved music types by me and because of that I usually come back to bands like: Talk Talk, New Order, OMD, Bronski Beat. And because I am geek it is obvious that second track on my list is \"Revenge of the Nerds OST\"",
+        after_movies: "Movies",
+        after_movies_p: "As you probably saw in my short bio. I do love gangster movies. Scarface (1983) I love most. But besides of that general rule which is going through my mind when I choose any movie is the fact that movie is \"about something\" - just INTJ things. Dramas, comedies, thrillers or based on real life stories let me feel entertainment.",
+        after_books: "Books",
+        after_books_p: "This paragraph because of me being nerd and geek should be long. But need of being strict and fully pragmatic will let me mention only types of books I like. More about titles I hope you will find in my blog. So, most beloved genres of books are also stronger like economy, political and obviously programming books same as a little lighter like fantasy books.",
+        contact_title: "Contact me",
+        contact_q: "Want to talk with me?",
+        contact_p: "Meet me on my social media or send me an email!",
+        footer: "Copyright by Piotr Retmańczyk",
+        stat_age: "Age",
+        stat_tool: "Fav Tool",
+        stat_amateur: "Amateur Years",
+        stat_movie: "Fav Movie",
+        stat_lang: "Fav Language",
+        stat_commercial: "Commercial Years"
+    },
+    pl: {
+        nav_about: "O mnie",
+        nav_after: "Po godzinach",
+        nav_contact: "Kontakt",
+        hero_hi: "Cześć! Dobrze, że jesteś!",
+        hero_welcome: "Witaj na mojej stronie!!",
+        about_title: "O mnie",
+        about_who: "Kim jestem?",
+        about_p1: "Cześć! Nazywam się Piotr Retmańczyk i od kilkunastu lat jestem programistą. W codziennej pracy moja rola to Senior Software Engineer",
+        about_p2: "Praca jako programista to mój wybór, moja pasja i tego chcę w życiu codziennym. Nic nie przynosi mi tyle frajdy co pisanie kodu. Złapałem bakcyla, kiedy uczyłem się na studiach inżynierskich. Programowanie to to, co kocham robić, a doskonalenie umiejętności, które są ściśle związane z moją pasją, to jedna z najważniejszych rzeczy w moim życiu. Moja podróż rozpoczęła się w 2011 roku od skryptów Pythona i po tym czasie przez lata miałem okazję pracować z wieloma technologiami i językami.",
+        about_day_title: "Dzień po dniu",
+        about_day_p: "W codziennym programowaniu używam języków takich jak Java czy JS a ostatnio również Golang, które w moim przypadku są ściśle powiązane z backendem. Przeważnie moja kariera opiera się na byciu typowym backend deweloperem. Ale nie mówię „nie” żadnym technologiom, które pozwalają mi tworzyć aplikacje mobilne, desktopowe i webowe.",
+        after_title: "Po godzinach",
+        after_q: "Co robię po pracy?",
+        after_p1: "Jak sugerują największe umysły branży \"IT\" - cały czas doskonalę zestaw języków, umiejętności miękkie i umiejętność tworzenia rozwiązań architektonicznych.",
+        after_p2: "Znalazłem też sposób na świetną rozrywkę i hobby w tworzeniu gier. Nie mam dużego sukcesu na moim koncie, ale możesz znaleźć pewien tytuł w Internecie. Jeśli jesteś zainteresowany, sprawdź <a href=\"https://seriousappsdev.itch.io/mirrorreflection\" target=\"_blank\">tę stronę</a>",
+        after_hobby: "Hobby i zainteresowania",
+        after_music: "Muzyka",
+        after_music_p1: "Jestem wielkim entuzjastą muzyki z lat 70,80,90. Mogę tu zamieścić długą listę zespołów, ale jest tak długa, że zostanę z kilkoma najbardziej lubianymi przeze mnie.",
+        after_music_p2: "New Wave, Synth Pop i Rock to przeze mnie najbardziej lubiane rodzaje muzyki, dlatego zazwyczaj wracam do takich zespołów jak: Talk Talk, New Order, OMD, Bronski Beat. A ponieważ jestem nerdem jest oczywiste, że drugi utwór na mojej liście to „Revenge of the Nerds OST”",
+        after_movies: "Filmy",
+        after_movies_p: "Jak pewnie widzieliście w mojej krótkiej biografii. Uwielbiam filmy gangsterskie. Scarface (1983) kocham najbardziej. Ale poza tym ogólną zasadą, która przychodzi mi do głowy, kiedy wybieram dowolny film, jest fakt że ten film jest „o czymś” - takie tam sprawy typowego INTJ. Dramaty, komedie, thrillery lub oparte na prawdziwych historiach pozwalają mi poczuć w końcu odrobinę przyjemności.",
+        after_books: "Książki",
+        after_books_p: "Ten akapit, ponieważ jestem geekiem i nerdem, powinien być długi. Ale potrzeba bycia w pełni pragmatycznym pozwoli mi wymienić tylko te rodzaje książek, które lubię. Więcej o tytułach mam nadzieję, że znajdziecie na moim blogu, którego planuję stworzyć pewnego pięknego dnia. Tak więc najbardziej lubiane przeze mnie książki to takie jak tytuły ekonomiczne, polityczne i oczywiście programistyczne. Nie czytam tylko \"trudnej literatury\". Jestem również wielkim fanem fantasy.",
+        contact_title: "Kontakt",
+        contact_q: "Masz ochotę o coś zapytać?",
+        contact_p: "Pisz śmiało na moje konta w serwisach społecznościowych! Czekam również na mail!",
+        footer: "Copyright by Piotr Retmańczyk",
+        stat_age: "Wiek",
+        stat_tool: "Ulubione narzędzie",
+        stat_amateur: "Lata amatorskie",
+        stat_movie: "Ulubiony film",
+        stat_lang: "Ulubiony język",
+        stat_commercial: "Lata komercyjne"
+    }
+};
+
+function updateStats() {
+    const curYear = new Date().getFullYear();
+    const wasBorn = curYear - 1991;
+    const amateurYears = curYear - 2012;
+    const commercial = curYear - 2014;
+
+    document.querySelectorAll('[data-stat]').forEach(el => {
+        const stat = el.getAttribute('data-stat');
+        if (stat === 'born') el.textContent = wasBorn;
+        if (stat === 'amateur') el.textContent = amateurYears;
+        if (stat === 'commercial') el.textContent = commercial;
+        if (stat === 'cur-year') el.textContent = curYear;
+    });
+}
+
+function updateThemeByTime() {
+    const hour = new Date().getHours();
+    const isDayTime = hour >= 6 && hour < 18;
+
+    if (isDayTime) {
+        document.body.classList.add('light-theme');
+        document.body.classList.remove('dark-theme');
+    } else {
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
+    }
+}
+
+function setLanguage(lang) {
+    localStorage.setItem('preferredLang', lang);
+    
+    document.querySelectorAll('[data-i18n]').forEach(el => {
+        const key = el.getAttribute('data-i18n');
+        if (translations[lang][key]) {
+            el.innerHTML = translations[lang][key];
+        }
+    });
+
+    document.querySelectorAll('.lang-btn').forEach(btn => {
+        btn.classList.toggle('active', btn.getAttribute('onclick').includes(lang));
+    });
+
+    document.documentElement.lang = lang;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedLang = localStorage.getItem('preferredLang') || (navigator.language.startsWith('pl') ? 'pl' : 'en');
+    setLanguage(savedLang);
+    updateStats();
+    updateThemeByTime();
+    // Sprawdzaj motyw co minutę, w razie gdyby użytkownik zostawił otwartą stronę
+    setInterval(updateThemeByTime, 60000);
+});
